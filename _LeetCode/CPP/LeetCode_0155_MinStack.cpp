308624f6001e7c13767374d74a7ad17804f75448
LearnerHB <875352427@qq.com>
2021-07-19 19:37:49 +0800

    Add LeetCode_0155_MinStack.cpp

---
+++ _LeetCode/CPP/LeetCode_0155_MinStack.cpp
@@ -0,0 +1,51 @@
+/*  最小栈
+设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+
+push(x) —— 将元素 x 推入栈中。
+pop()—— 删除栈顶的元素。
+top()—— 获取栈顶元素。
+getMin() —— 检索栈中的最小元素。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/min-stack
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+*/
+
+// Sol
+class MinStack {
+    stack<int> x_stack;
+    stack<int> min_stack;   
+public:
+    /** initialize your data structure here. */
+    MinStack() {
+        min_stack.push(INT_MAX);
+    }
+    
+    void push(int val) {
+        x_stack.push(val);
+        min_stack.push(min(min_stack.top(), val));
+    }
+    
+    void pop() {
+        x_stack.pop();
+        min_stack.pop();
+    }
+    
+    int top() {
+        return x_stack.top();
+    }
+    
+    int getMin() {
+        return min_stack.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
