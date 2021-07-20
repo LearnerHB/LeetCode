e05ce3cedd0bf9bcb1fa0fc374078c0356270c03
LearnerHB <875352427@qq.com>
2021-07-20 23:26:18 +0800

    Add another solution of LeetCode_0206_reverseList.cpp

--- _LeetCode/CPP/LeetCode_0206_reverseList.cpp
+++ _LeetCode/CPP/LeetCode_0206_reverseList.cpp
@@ -28,3 +28,16 @@ public:
         return prev;
     }
 };
+
+// Sol2 递归 
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr)
+            return head;
+        ListNode* p = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+        return p;
+    }
+};
